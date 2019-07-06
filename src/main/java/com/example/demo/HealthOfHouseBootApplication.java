e7c728fb89d61f9aa6690b04e4cdcdc36a9f76df
小伟 <1282927364@qq.com>
2019-07-07 00:01:20 +0800

    g

--- src/main/java/com/example/demo/HealthOfHouseBootApplication.java
+++ src/main/java/com/example/demo/HealthOfHouseBootApplication.java
@@ -2,15 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
-@RequestMapping("/xiaosheng")
-public class HealthOfHouseBootApplication extends SpringBootServletInitializer{
+public class HealthOfHouseBootApplication{
 
 	//进入登录页面
 	@RequestMapping("/")
@@ -22,11 +19,5 @@ public class HealthOfHouseBootApplication extends SpringBootServletInitializer{
 		SpringApplication.run(HealthOfHouseBootApplication.class, args);
 	}
 	
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return builder.sources(HealthOfHouseBootApplication.class);
-	}
 	
 }
