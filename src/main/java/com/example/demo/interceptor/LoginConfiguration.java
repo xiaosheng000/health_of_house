ba1a373e7456218ed5487deee46969cf2933d38b
小伟 <1282927364@qq.com>
2019-07-06 23:47:09 +0800

    g

--- src/main/java/com/example/demo/interceptor/LoginConfiguration.java
+++ src/main/java/com/example/demo/interceptor/LoginConfiguration.java
@@ -20,7 +20,7 @@ public class LoginConfiguration implements WebMvcConfigurer{
 				.addPathPatterns("/**") //
 				//除了哪些路径
 				.excludePathPatterns("/xiaosheng/login","/xiaosheng/tologin","/xiaosheng","/","/favicon.ico", //
-									"/css/**","/js/**","/xiaosheng/images/**","/fonts/**");
+									"/xiaosheng/css/**","/xiaosheng/js/**","/xiaosheng/images/**","/xiaosheng/fonts/**");
 		
 	}
 
